182aaefbb5378449325f3b1b2a29f03c847026b4
vanderlivrlj <vanderlidev@gmail.com>
2023-06-04 18:39:57 -0300

    Criando controller, repository e entidade do course

--- src/main/java/com/vanderli/CrudSpringApplication.java
+++ src/main/java/com/vanderli/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.vanderli;
 
+import com.vanderli.model.Course;
+import com.vanderli.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -9,5 +13,19 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring.");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
+
 
 }
